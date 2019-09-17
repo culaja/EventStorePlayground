ae297343ac992a0874bb6c4a654d91c1539ea18a
Stanko Culaja <culaja@gmail.com>
2019-09-17 22:25:44 +0200

    UserId is now email.

--- Domain/UserId.cs
+++ Domain/UserId.cs
@@ -1,16 +1,20 @@
+using System.Text.RegularExpressions;
 using Common;
 
 namespace Domain
 {
     public sealed class UserId : AggregateId
     {
+        private static readonly Regex EmailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        
         private UserId(string id) : base(nameof(User), id)
         {
         }
 
         public static UserId UserIdFrom(Maybe<string> maybeId) => maybeId
             .IfNullOrWhitespace(() => throw new InvalidUserIdException($"{nameof(UserId)} can't be null, empty or white space string."))
-            .Map(id => new UserId(id)).Value;
+            .Map(id => EmailRegex.Match(id).Success ? id : throw new InvalidUserIdException($"{nameof(UserId)} has to be valid e-mail address."))
+            .Map(email => new UserId(email)).Value;
 
         public static implicit operator string(UserId userId) => userId.ToString();
     }
--- UnitTests/ValueObjectValidationTests/UserIdTests.cs
+++ UnitTests/ValueObjectValidationTests/UserIdTests.cs
@@ -11,12 +11,14 @@ namespace UnitTests.ValueObjectValidationTests
         [InlineData(null)]
         [InlineData("")]
         [InlineData("   ")]
+        [InlineData("culaja")]
+        [InlineData("john.doe")]
         public void for_invalid_input_throws_InvalidUserIdException(string userIdString) =>
             Assert.Throws<InvalidUserIdException>(() => UserIdFrom(userIdString));
 
         [Theory]
         [InlineData("culaja@gmail.com")]
-        [InlineData("john.doe")]
+        [InlineData("john.doe@example.com")]
         public void for_valid_input_valid_UserId_is_created(string userIdString) =>
             UserIdFrom(userIdString).ToString().Should().Be(userIdString);
     }
